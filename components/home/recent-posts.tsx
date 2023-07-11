2a8ee8f391a7515b4cd5c921e8fd6a8bb0cc3adf
unknown <kd03102014@gmail.com>
2023-07-11 14:56:13 +0700

    update project

---
+++ components/home/recent-posts.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface RecentPostsProps {
+}
+
+export function RecentPosts (props: RecentPostsProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
